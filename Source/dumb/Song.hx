93ac9eef8f2680337bfe59d9de43b74fa0c18fd3
Devin503 <devinpschweiss@gmail.com>
2022-04-06 21:11:59 -0400

    THESE ARE LITERALLY FROM FNF. LMAO

---
+++ Source/dumb/Song.hx
@@ -0,0 +1,136 @@
+package dumb;
+
+import dumb.Section.SusSection;
+import haxe.Json;
+import haxe.format.JsonParser;
+import lime.utils.Assets;
+import sys.io.File;
+import sys.FileSystem;
+using StringTools;
+
+/**THIS IS A DIRECT COPY FROM FNF PSYCH ENGINE. I AM LAZY RN
+    @since 0.0.3 (early April 2022)
+*/
+typedef SusSong =
+{
+	var song:String;
+	var notes:Array<SusSection>;
+	var events:Array<Dynamic>;
+	var bpm:Float;
+	var needsVoices:Bool;
+	var speed:Float;
+
+	var player1:String;
+	var player2:String;
+	var player3:String; //deprecated, now replaced by gfVersion
+	var gfVersion:String;
+	var stage:String;
+
+	var arrowSkin:String;
+	var splashSkin:String;
+	var validScore:Bool;
+}
+
+class Song
+{
+	public var song:String;
+	public var notes:Array<SusSection>;
+	public var events:Array<Dynamic>;
+	public var bpm:Float;
+	public var needsVoices:Bool = true;
+	public var arrowSkin:String;
+	public var splashSkin:String;
+	public var speed:Float = 1;
+	public var stage:String;
+
+	public var player1:String = 'bf';
+	public var player2:String = 'dad';
+	public var player3:String = 'gf'; //deprecated
+	public var gfVersion:String = 'gf';
+
+	private static function onLoadJson(songJson) // Convert old charts to newest format
+	{
+		if(songJson.gfVersion == null)
+		{
+			songJson.gfVersion = songJson.player3;
+			songJson.player3 = null;
+		}
+
+		if(songJson.events == null)
+		{
+			songJson.events = [];
+			for (secNum in 0...songJson.notes.length)
+			{
+				var sec:SusSection = songJson.notes[secNum];
+
+				var i:Int = 0;
+				var notes:Array<Dynamic> = sec.sectionNotes;
+				var len:Int = notes.length;
+				while(i < len)
+				{
+					var note:Array<Dynamic> = notes[i];
+					if(note[1] < 0)
+					{
+						songJson.events.push([note[0], [[note[2], note[3], note[4]]]]);
+						notes.remove(note);
+						len = notes.length;
+					}
+					else i++;
+				}
+			}
+		}
+	}
+
+	public function new(song, notes, bpm)
+	{
+		this.song = song;
+		this.notes = notes;
+		this.bpm = bpm;
+	}
+
+	public static function loadFromJson(jsonInput:String, ?folder:String):SusSong
+	{
+		var rawJson = null;
+		
+		var formattedFolder:String = 'Assets/JsonData/Songs/$folder/';
+		var formattedSong:String = jsonInput.toLowerCase().replace(' ', '-') + '.json';
+
+		if(rawJson == null) {
+			rawJson = File.getContent(formattedFolder + formattedSong).trim();
+		}
+
+		while (!rawJson.endsWith("}"))
+		{
+			rawJson = rawJson.substr(0, rawJson.length - 1);
+			// LOL GOING THROUGH THE BULLSHIT TO CLEAN IDK WHATS STRANGE
+		}
+
+		// FIX THE CASTING ON WINDOWS/NATIVE
+		// Windows???
+		// trace(songData);
+
+		// trace('LOADED FROM JSON: ' + songData.notes);
+		/* 
+			for (i in 0...songData.notes.length)
+			{
+				trace('LOADED FROM JSON: ' + songData.notes[i].sectionNotes);
+				// songData.notes[i].sectionNotes = songData.notes[i].sectionNotes
+			}
+
+				daNotes = songData.notes;
+				daSong = songData.song;
+				daBpm = songData.bpm; */
+
+		var songJson:SusSong = parseJSONshit(rawJson);
+		//if(jsonInput != 'events') StageData.loadDirectory(songJson);
+		onLoadJson(songJson);
+		return songJson;
+	}
+
+	public static function parseJSONshit(rawJson:String):SusSong
+	{
+		var swagShit:SusSong = cast Json.parse(rawJson).song;
+		swagShit.validScore = true;
+		return swagShit;
+	}
+}
