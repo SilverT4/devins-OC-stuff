02542d8c4bd0ca3b520ac410fa908a54ecd29000
Devin503 <devinpschweiss@gmail.com>
2022-04-06 21:33:21 -0400

    you are goin to brazil

--- Source/characterUtils/CharacterPicture.hx
+++ Source/characterUtils/CharacterPicture.hx
@@ -7,14 +7,17 @@ using StringTools;
 
 class CharacterPicture extends FlxSprite {
     public var MyURL:String = '';
-    public function new(x:Float = 0, y:Float = 0, imagePath:String, ?URLPath:String) {
+    public var funnyTrace:String = '';
+    public function new(x:Float = 0, y:Float = 0, imagePath:String, ?URLPath:String, ?funnyTrace:String) {
         super(x, y);
         loadGraphic((imagePath.contains('/')) ? imagePath : FileUtils.pic(imagePath));
         if (URLPath != null) this.MyURL = URLPath;
+        if (funnyTrace != null) this.funnyTrace = funnyTrace;
     }
 
     public function openMyURL() {
-        if (MyURL != null) {
+        if (MyURL != null && MyURL.length > 1) {
+            if (funnyTrace.length >= 1) trace(funnyTrace);
             SusUtil.openLink(MyURL);
         }
     }
