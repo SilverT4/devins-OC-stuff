a065f7963f18df3e96d68dba7027e67b4f76b603
Devin503 <devinpschweiss@gmail.com>
2022-04-08 11:11:16 -0400

    rework!!

--- Source/FileUtils.hx
+++ Source/FileUtils.hx
@@ -1,209 +1,119 @@
 package;
 
-import flixel.input.keyboard.FlxKey;
-import haxe.Json;
-import sys.FileSystem;
-import sys.io.File as SusFile;
-import sys.FileStat;
-import IntroStuff;
-import flixel.graphics.frames.FlxAtlasFrames as Faf;
-import characterUtils.CharacterThings;
-import haxe.ValueException;
-import flixel.animation.FlxAnimationController;
+import openfl.Assets;
+import openfl.utils.AssetType;
+import flixel.*;
+import haxe.Exception;
+import flixel.graphics.frames.FlxAtlasFrames;
 using StringTools;
-/**File utilities. Allows you to grab files or check if they exist.
-    
-TODO: Add save/load capabilities.
-@since 0.0.1*/
+
+/**Rework of the FileUtils class. Started in early April 2022. It'll *hopefully* be more compatible with the limitations of HTML5 (I'm writing this file with HTML5 mode in VS Code), even if that means on-the-fly asset access is eliminated.
+
+(Perhaps I'll make some functions for both HTML5 and sys builds so I can have on-the-fly access in desktop/mobile builds?)
+    @since 0.0.3 (early April 2022)*/
 class FileUtils {
-    static final bruh = IntroStuff.sndExtension[IntroStuff.yourPlatform];
-    static final defaultPaths:Map<String, String> = [
-        'image' => 'Assets/Images/',
-        'audio' => 'Assets/Audio/',
-        'chara' => 'Assets/Characters/',
-        'xml' => 'Assets/Images/',
-        'bird' => 'Assets/Images/',
-        'json' => 'Assets/JsonData/',
-        'txt' => 'Assets/TxtData/'
-    ];
-    static final commonExtensions:Map<String, String> = [
-        'image' => '.png',
-        'chara' => '.character',
-        'xml' => '.xml',
-        'json' => '.json'
-    ];
-    public static var muteKeys:Array<FlxKey> = [FlxKey.ZERO, FlxKey.NUMPADZERO];
-    public static var volKeysUp:Array<FlxKey> = [FlxKey.PLUS, FlxKey.NUMPADPLUS];
-    public static var volKeysDown:Array<FlxKey> = [FlxKey.MINUS, FlxKey.NUMPADMINUS];
-    /**Checks if the file exists. The file must have an extension, if it doesn't the game will crash!!
-    @since 0.0.1*/
-    public static function existential(File:String, ?Path:String):Bool {
-        var yes:Bool = false;
-        if (Path != null) {
-            if (FileSystem.exists(Path + File)) {
-                yes = true;
-            }
+    static final DefaultImagePath = "Assets/Images/"; // With the second `/` there, you can just type "DefaultImagePath + WHATEVER" lol
+    static final DefaultAudioPath = "Assets/Audio/";
+    static final DefaultCharacterPath = "Assets/Characters/";
+    static final DefaultJsonPath = "Assets/JsonData/";
+    static final DefaultTextPath = "Assets/TxtData/";
+
+    public static function fileExists(FilePath:String, ?Directory:String) {
+        if (Directory != null) {
+            if (Assets.exists(Directory + FilePath)) return true;
+            else if (Assets.exists(fuckinDefaultShit(FilePath))) return true;
+            else return false;
         } else {
-            var ben = File.split('.');
-            var exten = ben[1];
-            var neb = bruh.replace('.', '');
-            switch (exten) {
-                case neb:
-                    if (FileSystem.exists(defaultPaths[audio] + File)) {
-                        yes = true;
-                    }
-                case 'json':
-                    if (FileSystem.exists(defaultPaths[json] + File)) {
-                        yes = true;
-                    }
-                case 'character':
-                    if (FileSystem.exists(defaultPaths[chara] + File)) {
-                        yes = true;
-                    }
-                case 'png' | 'xml':
-                    if (FileSystem.exists(defaultPaths[image] + File)) {
-                        yes = true;
-                    }
-                case 'txt':
-                    if (FileSystem.exists(defaultPaths[txt] + File)) {
-                        yes = true;
-                    }
-            }
+            if (Assets.exists(fuckinDefaultShit(FilePath))) return true;
+            else return false;
         }
-        return yes;
-    }
-    /**Allows you to load a picture. No extension needed as this function will return that along with the path.
-    @since 0.0.1*/
-    public static function pic(File:String, ?Path:String):String {
-        var pathThingy = 'Assets/Images/';
-        var fileThingy = File + commonExtensions[image];
-        if (Path != null) pathThingy = Path;
-        if (Path != null) {
-            if (existential(File + commonExtensions[image], Path)) {
-                imageFileThing = Path + File + commonExtensions[image];
-            }
-        } else {
-            if (existential(File + commonExtensions[image])) {
-                imageFileThing = defaultPaths[image] + File + commonExtensions[image];
-            }
-        }
-        return pathThingy + fileThingy;
-    }
-    /**Allows you to load audio. No extension needed as this function will return that along with the path.
-    @since 0.0.1*/
-    public static function sound(File:String, ?Path:String):String {
-        var audioFileThing:String = '';
-        if (Path != null) {
-            if (existential(File + bruh, Path)) {
-                audioFileThing = Path + File + bruh;
-            }
-        } else {
-            if (existential(File + bruh)) {
-                audioFileThing = defaultPaths[audio] + File + bruh;
-            }
-        }
-        return audioFileThing;
-    }
-    /**Allows you to load a spritesheet. No extension needed for either the image or XML, as this function will return the FlxAtlasFrames as needed.
-    @since 0.0.1*/
-    public static function birdy(File:String, ?Path:String):Array<String> {
-        var pathThingy = 'Assets/Images/';
-        if (Path != null) pathThingy = Path;
-        var imageyShit:String = pathThingy + File + '.png';
-        var xmlShit:String = pathThingy + File + '.xml';
-        /*if (Path != null) {
-            if (existential(File + commonExtensions[image], Path) && existential(File + commonExtensions[xml], Path)) {
-                imageyShit = Path + File + commonExtensions[image];
-                xmlShit = Path + File + commonExtensions[xml];
-                return [imageyShit, xmlShit];
-            }
-        } else {
-            if (existential(File + commonExtensions[image]) && existential(File + commonExtensions[xml])) {
-                imageyShit = defaultPaths[image] + File + commonExtensions[image];
-                xmlShit = defaultPaths[xml] + File + commonExtensions[xml];
-                return [imageyShit, xmlShit];
-                }
-        } */ // may need this later!
-        trace(imageyShit, xmlShit);
-        trace(File);
-        if (Path != null) trace(Path);
-        return [imageyShit, xmlShit];
-        // return Faf.fromSparrow(imageFileThing, xmlFileThing);
-    }
-    /**Uses birdy's output to give frames
-    @since 0.0.1*/
-    public static function getBirdAtlas(Bird:Array<String>) {
-        return Faf.fromSparrow(Bird[0], Bird[1]);
-    }
-    /**Allows you to load the character's properties.
-    @since 0.0.1*/
-    public static function charProps(File:String):CharProperties {
-        if (existential(File + commonExtensions[chara])) {
-            charaFileThing = defaultPaths[chara] + File + commonExtensions[chara];
-        }
-        return cast Json.parse(getDataInt(charaFileThing));
-    }
-    /**Reads data from a file. (INTERNAL THING THAT ALREADY HAS PATHS SET UP! THERE'S A SEPARATE ONE FOR USE IN OTHER CLASSES.)
-    @since 0.0.1*/
-    inline static function getDataInt(FilePath:String):String {
-        return SusFile.getContent(FilePath);
     }
 
-    /**Reads data from a file. (**USE THIS IN YOUR CLASSES TO AVOID ERRORS ABOUT A PRIVATE FUNCTION!**)
-    @param File The file to read
-    @param FileType Is it an **xml** (0), **json** (1), **character** (2), or a **txt** file (3)? (Only necessary if Path is nulL!)
-    @param Path (Optional) The file path.
-    @since 0.0.1*/
-    public static function getFileData(File:String, ?Path:String, ?FileType:Int):String {
-        if (Path != null) {
-            dataFileThing = SusFile.getContent(Path + File);
-        } else {
-            switch (FileType) {
-                case 2:
-                    dataFileThing = SusFile.getContent(defaultPaths[chara] + File + commonExtensions[xml]);
-                case 1:
-                    dataFileThing = SusFile.getContent(defaultPaths[json] + File + commonExtensions[json]);
-                case 3:
-                    dataFileThing = SusFile.getContent(defaultPaths[txt] + File + '.txt');
-                case 0:
-                    dataFileThing = SusFile.getContent(defaultPaths[xml] + File + commonExtensions[xml]); // you could really just use readXmlAnims since it's public. lmfao
-            }
+    static function fuckinDefaultShit(FileName:String) {
+        var plz = FileName.split('.');
+        var EXTENSION_SHIT = plz[plz.length - 1]; // in case there's multiple dots!!
+        switch (EXTENSION_SHIT) {
+            case 'png' | 'xml':
+                return DefaultImagePath + FileName;
+            case 'txt':
+                return DefaultTextPath + FileName;
+            case 'json':
+                return DefaultJsonPath + FileName;
+            case 'character':
+                return DefaultCharacterPath + FileName;
+            case #if !web 'ogg' #else 'mp3' #end:
+                return DefaultAudioPath + FileName;
+            default:
+                throw new Exception("Unable to figure out the asset type for " + FileName + ".");
         }
-        return dataFileThing;
-    }
-    /**CharacterThings will read this if no file exists for the specified character.
-    @since 0.0.1*/
-    public static function readXmlAnims(File:String):Dynamic {
-        var mama = cast Xml.parse(getDataInt(defaultPaths[xml] + File + commonExtensions[xml]));
-        return mama;
-    }
-    /**Copy a file from outside of the game's folders to your game.
-    @param File Where the file is
-    @param Path Where to place it.
-    @since 0.0.1*/
-    public static function externalFile(File:String, Path:String) {
-        new sys.io.Process('cmd', ['/c copy ' + File, Path]);
-    }
-    /**Allows you to quickly parse a template and save a few seconds typing it out.
-        @since 0.0.2
-        @param Template The template to parse.*/
-    public static function parseTemplate(Template:Dynamic):Dynamic {
-        return Json.parse(Json.stringify(Template));
     }
 
-    public static function convertToFile(JsonData:Dynamic):String {
-        return Json.stringify(JsonData, "\t");
+    /**Get an image.
+        
+    @param FileName The file's name.
+    @param Directory (Optional) Look in this directory. If it's not found there, an attempt will be made to search the default image path.
+    @param HasExtension If `FileName` has `.png` at the end, set this to true. Otherwise, set it to false.
+    @since 0.0.3 (early April 2022)*/
+    public static function image(FileName:String, ?Directory:String, HasExtension:Bool = true) {
+        var pissyShit = (HasExtension) ? FileName : FileName + '.png';
+        if (Directory != null) {
+            if (fileExists(pissyShit, Directory)) return Directory + pissyShit;
+            else if (fileExists(pissyShit, DefaultImagePath)) return DefaultImagePath + pissyShit;
+            else throw new Exception("Unable to locate " + FileName + ".");
+        } else {
+            if (fileExists(pissyShit, DefaultImagePath)) return DefaultImagePath + pissyShit;
+            else throw new Exception("Unable to locate " + FileName + "."); // I'll be removing these exceptions at some point.
+        }
     }
-    static var dataFileThing:String;
-    static var charaFileThing:String;
-    static var xmlFileThing:String;
-    static var imageFileThing:String;
-    //static var audioFileThing:String;
-    static final image:String = 'image';
-    static final audio:String = 'audio';
-    static final xml:String = 'xml';
-    static final bird:String = 'bird';
-    static final chara:String = 'chara';
-    static final json:String = 'json';
-    static final txt:String = 'txt';
+
+    #if web
+    /**Get an audio file.
+        
+    @param FileName The file's name.
+    @param Directory (Optional) Look in this directory. If it's not found there, an attempt will be made to search the default audio path.
+    @param HasExtension If `FileName` has `.mp3` at the end, set this to true. Otherwise, set it to false.
+    @since 0.0.3 (early April 2022)*/
+    #else
+    /**Get an audio file.
+        
+    @param FileName The file's name.
+    @param Directory (Optional) Look in this directory. If it's not found there, an attempt will be made to search the default audio path.
+    @param HasExtension If `FileName` has `.ogg` at the end, set this to true. Otherwise, set it to false.
+    @since 0.0.3 (early April 2022)*/
+    #end
+    public static function sound(FileName:String, ?Directory:String, HasExtension:Bool = true) {
+        var shitBurger = (HasExtension) ? FileName : FileName + #if web '.mp3' #else '.ogg' #end;
+        if (Directory != null) {
+            if (fileExists(shitBurger, Directory)) return Directory + shitBurger;
+            else if (fileExists(shitBurger)) return DefaultAudioPath + shitBurger;
+            else return DefaultAudioPath + "errorSound." + #if web 'mp3' #else 'ogg' #end;
+        } else {
+            if (fileExists(shitBurger)) return DefaultAudioPath + shitBurger;
+            else return DefaultAudioPath + "errorSound." + #if web 'mp3' #else 'ogg' #end;
+        }
+    }
+
+    public static function getFont(FontName:String, HasExtension:Bool = true) {
+        var pissyShit = (HasExtension) ? FontName : FontName + '.ttf';
+        if (Assets.exists("Assets/Fonts/" + pissyShit)) return 'Assets/Fonts/$pissyShit';
+        else return '_sans';
+    }
+
+    public static function birdy(ImageName:String, ?Directory:String) {
+        if (Directory != null) {
+            if (fileExists(ImageName + '.png', Directory) && fileExists(ImageName + '.xml', Directory)) {
+                var peter = Directory + ImageName;
+                return ['$peter.png', '$peter.xml'];
+            } else return ["Assets/Images/speen.png", "Assets/Images/speen.xml"];
+        } else {
+            if (fileExists(ImageName + '.png') && fileExists(ImageName + '.xml')) {
+                var peter = DefaultImagePath + ImageName;
+                return ['$peter.png', '$peter.xml'];
+            } else return ["Assets/Images/speen.png", "Assets/Images/speen.xml"];
+        }
+    }
+
+    public static function getBirdAtlas(BIRDY:Array<String>) {
+        return FlxAtlasFrames.fromSparrow(BIRDY[0], BIRDY[1]);
+    }
 }
